1d6d3b0682b15b1fa947d09d0e8a02667ea32783
lorraineyul <lorraineyu.y@gmail.com>
2023-09-25 02:14:31 +0100

    first

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import PostCreat from "./PostCreat";
+import PostList from "./PostList";
+
+export default () => {
+  return (
+    <div className="container">
+      <h1>Create Post</h1>
+      <PostCreat />
+      <hr />
+      <h1>Posts</h1>
+      <PostList />
+    </div>
+  );
+};
